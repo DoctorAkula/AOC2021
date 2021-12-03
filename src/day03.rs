f69c0c49d8f3a361ad7a367e4a8646bbcc72180f
Bob <atomicsteameng@gmail.com>
2021-12-03 00:23:49 -0800

    Removed duplication from Day 3 Part 2

--- src/day03.rs
+++ src/day03.rs
@@ -25,68 +25,45 @@ pub fn day03p1() {
     println!("Product: {}", gamma * epsilon);
 }
 
+fn bitsieve(mut input_clone : Vec<String>, bitlen : usize, cmpfunc : fn(usize, usize) -> bool) -> String {
+    for ind in 0..bitlen {
+        let mut zerocount = 0;
+        let mut onecount = 0;
+        for line in &input_clone {
+            let line = line.as_bytes();
+            if line[ind] == '0' as u8 {
+                zerocount += 1;
+            }else if line[ind] == '1' as u8 {
+                onecount += 1;
+            }
+        }
+
+        let comp = if cmpfunc(onecount, zerocount) {
+            '1' as u8
+        }else{
+            '0' as u8
+        };
+
+        input_clone = input_clone.into_iter().filter(|line| {
+            let line = line.as_bytes();
+            line[ind] == comp
+        }).collect();
+
+        if input_clone.len() == 1 {
+            break;
+        }
+    }
+    input_clone[0].clone()
+}
+
 pub fn day03p2() {
     let input = loadinput("./input/day3.txt");
     let bitlen = input[0].len();
-    let mut input_clone = input.clone();
-    for ind in 0..bitlen {
-        let mut zerocount = 0;
-        let mut onecount = 0;
-        for line in &input_clone {
-            let line = line.as_bytes();
-            if line[ind] == '0' as u8 {
-                zerocount += 1;
-            }else if line[ind] == '1' as u8 {
-                onecount += 1;
-            }
-        }
+    let input_clone = input.clone();
+    let oxygen = usize::from_str_radix(&bitsieve(input_clone, bitlen, |left, right| left >= right), 2).unwrap();
 
-        let comp = if onecount >= zerocount {
-            '1' as u8
-        }else{
-            '0' as u8
-        };
-
-        input_clone = input_clone.into_iter().filter(|line| {
-            let line = line.as_bytes();
-            line[ind] == comp
-        }).collect();
-
-        if input_clone.len() == 1 {
-            break;
-        }
-    }
-    let oxygen = usize::from_str_radix(&input_clone[0], 2).unwrap();
-
-    let mut input_clone = input.clone();
-    for ind in 0..bitlen {
-        let mut zerocount = 0;
-        let mut onecount = 0;
-        for line in &input_clone {
-            let line = line.as_bytes();
-            if line[ind] == '0' as u8 {
-                zerocount += 1;
-            }else if line[ind] == '1' as u8 {
-                onecount += 1;
-            }
-        }
-
-        let comp = if onecount < zerocount {
-            '1' as u8
-        }else{
-            '0' as u8
-        };
-
-        input_clone = input_clone.into_iter().filter(|line| {
-            let line = line.as_bytes();
-            line[ind] == comp
-        }).collect();
-
-        if input_clone.len() == 1 {
-            break;
-        }
-    }
-    let c02scrub = usize::from_str_radix(&input_clone[0], 2).unwrap();
+    let input_clone = input.clone();
+    let c02scrub = usize::from_str_radix(&bitsieve(input_clone, bitlen, |left, right| left < right), 2).unwrap();
 
     println!("Oxygen: {}", oxygen);
     println!("c02scrub: {}", c02scrub);
